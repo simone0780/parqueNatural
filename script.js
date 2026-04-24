90c8d2a902060b4c1570525bf38f15206ef1e94d
Ernesto lopez simon <simonernestolopez71080@gmail.com>
2026-04-24 12:35:46 +0200

    añadir_especie

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const imagenes = document.querySelectorAll("img");
 
-    imagenes.forEach((imagen) => {
-      imagen.classList.add("imagen-ampliable");
-      imagen.addEventListener("click", () => {
-        imagen.classList.toggle("ampliada");
-      });
-    });
+imagenes.forEach((imagen) => {
+  imagen.classList.add("imagen-ampliable");
+  imagen.addEventListener("click", () => {
+    const yaAmpliada = imagen.classList.contains("ampliada");
+    imagenes.forEach((img) => img.classList.remove("ampliada"));
+    if (!yaAmpliada) {
+      imagen.classList.add("ampliada");
+    }
+  });
+});
 
- 
